009422cc032e6518ec4b1105c637c9168e637bb7
G0nkDr0id395 <84355250+G0nkDr0id395@users.noreply.github.com>
2022-04-21 21:49:50 -0400

    Add files via upload

---
+++ deck.py
@@ -0,0 +1,53 @@
+import random
+from constant import *
+
+
+class Deck:
+    def __init__(self):
+        self.cards = []
+        self.build()
+
+    def build(self):
+        for value in RANKS:
+            for suit in SUITS:
+                self.cards.append((value, suit))
+
+    def shuffle(self):
+        random.shuffle(self.cards)
+
+    def deal(self):
+        if len(self.cards) > 1:
+            return self.cards.pop()
+
+
+class Hand(Deck):
+    def __init__(self):
+        self.cards = []
+        self.card_img = []
+        self.value = 0
+
+    def add_card(self, card):
+        self.cards.append(card)
+
+    def calc_hand(self):
+        first_card_index = [a_card[0] for a_card in self.cards]
+        non_aces = [c for c in first_card_index if c != 'a']
+        aces = [c for c in first_card_index if c == 'a']
+
+        for card in non_aces:
+            if card in 'jqk':
+                self.value += 10
+            else:
+                self.value += int(card)
+
+        for card in aces:
+            if self.value <= 10:
+                self.value += 11
+            else:
+                self.value += 1
+
+    def display_cards(self):
+        for card in self.cards:
+            cards = "".join((card[0], card[1]))
+            if cards not in self.card_img:
+                self.card_img.append(cards)
